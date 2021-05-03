d42f500e82803cc2eb27627014e4ca42fe45a0ca
Michelle Lefebvre <mmldesigns007@gmail.com>
2021-05-03 07:48:41 -0400

    added recipe components

--- recipe-shopping-list/src/app/recipes/recipes-list/recipes-list.component.ts
+++ recipe-shopping-list/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a test description', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_640.jpg'),
+    new Recipe('A Test Recipe', 'This is a test description', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_640.jpg'),
+    new Recipe('A Test Recipe', 'This is a test description', 'https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_640.jpg')
+  ];
 
   constructor() { }
 
